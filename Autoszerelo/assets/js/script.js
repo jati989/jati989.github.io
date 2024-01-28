f4bfd1d0c0b01fc91fbe99b5bdd543d08f98e45f
jati989 <125298988+jati989@users.noreply.github.com>
2024-01-28 17:01:48 +0100

    Add files via upload

---
+++ Autoszerelo/assets/js/script.js
@@ -0,0 +1,20 @@
+
+function navbarExiter() {
+    document.getElementById("navbarButton").click()
+};
+
+document.getElementById("navbarServices").addEventListener("click", function() {
+    navbarExiter();
+});
+
+document.getElementById("navbarIntroduction").addEventListener("click", function() {
+    navbarExiter();
+});
+
+document.getElementById("navbarRelationship").addEventListener("click", function() {
+    navbarExiter();
+});
+
+document.getElementById("navbarPrices").addEventListener("click", function() {
+    navbarExiter();
+});
